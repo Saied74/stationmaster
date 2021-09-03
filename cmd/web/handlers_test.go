7f6f39f765e3e827503430409c1e798ae2b3855a
Saied Seghatoleslami <seghatoleslami74@gmail.com>
2021-09-03 18:49:08 -0400

    added keyer start test

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -71,5 +73,67 @@ func TestKtutor(t *testing.T) {
 	}
 	if !bytes.Contains(body, []byte("Code Tutor and Keyer")) {
 		t.Errorf("expected body to contain Code Tutor and Keyer, did not get it")
+	}
+}
+
+type keyerPattern struct {
+	name      string
+	speed     string
+	farnspeed string
+	wsm       string
+	lsm       string
+	mode      string
+	contains  []string
+}
+
+func TestStart(t *testing.T) {
+	ktps := []keyerPattern{
+		{
+			name:      "normal keyer data",
+			speed:     "12",
+			farnspeed: "18",
+			wsm:       "1.2",
+			lsm:       "1.5",
+			mode:      "1",
+			contains:  []string{"12", "18", "1.2", "1.5", "1"},
+		},
+		{
+			name:      "bad data",
+			speed:     "abc",
+			farnspeed: "18",
+			wsm:       "1.g",
+			lsm:       "f.3",
+			mode:      "90",
+			contains:  []string{"Sending speed must be a number"},
+		},
+	}
+	for _, tp := range ktps {
+		app := newTestApp()
+		t.Run(tp.name, func(t *testing.T) {
+			body := strings.NewReader(fmt.Sprintf(
+				"speed=%s&farnspeed=%s&wsm=%s&lsm=%s&mode=%s",
+				tp.speed, tp.farnspeed, tp.wsm, tp.lsm, tp.mode))
+			r, err := http.NewRequest(http.MethodPost, "/", body)
+			if err != nil {
+				t.Fatal(err)
+			}
+			r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+			rr := httptest.NewRecorder()
+			app.start(rr, r)
+			rs := rr.Result()
+			defer rs.Body.Close()
+			if rs.StatusCode != http.StatusOK {
+				t.Errorf("expected %d got %d", http.StatusOK, rs.StatusCode)
+			}
+			b, err := io.ReadAll(rs.Body)
+			if err != nil {
+				t.Fatal(err)
+			}
+			for _, item := range tp.contains {
+				if !bytes.Contains(b, []byte(item)) {
+					t.Errorf("expected body to contain %s, did not get it", item)
+				}
+			}
+		})
 	}
 }
